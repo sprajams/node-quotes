d7b70997d66e0841bf17254eb9562a990a4c4d39
Suphasuta Prajamsri <suphprajam95@gmail.com>
2022-06-09 10:52:13 -0700

    install EJS as template engine

--- server.js
+++ server.js
@@ -1,28 +1,38 @@
-console.log("may node be with you");
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.listen(3000, function () {
-  console.log("listening on 3000");
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/quotes", (req, res) => {
-  console.log("bork");
-  console.log(req.body);
-});
-
 const MongoClient = require("mongodb").MongoClient;
 const connectionString =
   "mongodb+srv://suphpdev:aiqOygEnPL9tOUmd@cluster0.gwdon.mongodb.net/?retryWrites=true&w=majority";
-MongoClient.connect(connectionString, (err, client) => {
-  if (err) return console.error(err);
-  console.log("Connected to Database");
-});
+
+MongoClient.connect(connectionString)
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("mitm-quotes");
+    const quotesCollection = db.collection("quotes");
+
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get("/", (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.post("/quotes", (req, res) => {
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+
+    app.listen(3000);
+  })
+  .catch((error) => console.error(error));
